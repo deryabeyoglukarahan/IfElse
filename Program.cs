d567ac172ba51850867df0bb8fdbe7928ed051f7
deryabeyoglukarahan <derya.beyoglu1@gmail.com>
2025-05-09 11:24:04 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Lütfen bir sayi giriniz: ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+if (sayi > 10)
+{
+    Console.WriteLine("Girilen sayi 10'dan büyüktür.");
+}
+else
+{
+    Console.WriteLine("Girilen sayi 10'dan küçüktür.");
+}
+
+if (sayi % 2 == 0)
+{
+    Console.WriteLine("Girilen sayi çifttir.");
+}
+else
+{
+    Console.WriteLine("Girilen sayi tektir.");
+}
